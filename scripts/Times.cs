516a5ea9e07ea2985bb1419fd2d254fc5db779fe
Kido <63456333+the-kido@users.noreply.github.com>
2026-03-05 07:41:53 -0500

    chore: Update sleep time

--- scripts/Times.cs
+++ scripts/Times.cs
@@ -2,8 +2,8 @@ using Microsoft.VisualBasic;
 
 public static class Times
 {
-    const int REMINDER_MINUTE = (12 + 8) * 60 + 30;
-    const int MINUTE_I_SHOULD_BE_SHUT_DOWN = (12 + 9) * 60;
+    const int REMINDER_MINUTE = (12 + 8) * 60;
+    const int MINUTE_I_SHOULD_BE_SHUT_DOWN = (12 + 8) * 60 + 30;
 
     public static bool IsWarnTime() => DateAndTime.Now.Hour * 60 + DateAndTime.Now.Minute > REMINDER_MINUTE;
     public static bool IsSleepingTime() => DateAndTime.Now.Hour * 60 + DateAndTime.Now.Minute > MINUTE_I_SHOULD_BE_SHUT_DOWN;
